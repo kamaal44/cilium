27216ab857cb342622d19bcf2c57bfe0b2263c90
Martynas Pumputis <m@lambda.lt>
2020-03-19 09:43:02 +0100

    test: Extend testNodePort
    Extend the test function to accept a param which indicates whether
    tests from a third (aka node w/o cilium) node should be performed.
    
    As a reminder, the third node is used for NodePort testing to avoid
    bpf_sock.c optimizations which happen when a request is sent to
    a NodePort svc from a node which runs cilium.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -455,7 +455,7 @@ var _ = Describe("K8sServicesTest", func() {
 			return ipv4, ipv6
 		}
 
-		testNodePort := func(bpfNodePort bool, testSecondaryNodePortIP bool) {
+		testNodePort := func(bpfNodePort, testSecondaryNodePortIP, testFromOutside bool) {
 			var (
 				data                                 v1.Service
 				secondaryK8s1IPv4, secondaryK8s2IPv4 string
@@ -495,6 +495,10 @@ var _ = Describe("K8sServicesTest", func() {
 			tftpURL = getTFTPLink(k8s1IP, data.Spec.Ports[1].NodePort)
 			doRequests(httpURL, count, k8s1Name)
 			doRequests(tftpURL, count, k8s1Name)
+			if testFromOutside {
+				doRequestsFromThirdHost(httpURL, count, false)
+				doRequestsFromThirdHost(tftpURL, count, false)
+			}
 
 			httpURL = getHTTPLink("::ffff:"+k8s1IP, data.Spec.Ports[0].NodePort)
 			tftpURL = getTFTPLink("::ffff:"+k8s1IP, data.Spec.Ports[1].NodePort)
@@ -505,6 +509,10 @@ var _ = Describe("K8sServicesTest", func() {
 			tftpURL = getTFTPLink(k8s2IP, data.Spec.Ports[1].NodePort)
 			doRequests(httpURL, count, k8s1Name)
 			doRequests(tftpURL, count, k8s1Name)
+			if testFromOutside {
+				doRequestsFromThirdHost(httpURL, count, false)
+				doRequestsFromThirdHost(tftpURL, count, false)
+			}
 
 			httpURL = getHTTPLink("::ffff:"+k8s2IP, data.Spec.Ports[0].NodePort)
 			tftpURL = getTFTPLink("::ffff:"+k8s2IP, data.Spec.Ports[1].NodePort)
@@ -598,11 +606,19 @@ var _ = Describe("K8sServicesTest", func() {
 					tftpURL = getTFTPLink(secondaryK8s1IPv4, data.Spec.Ports[1].NodePort)
 					doRequests(httpURL, count, k8s1Name)
 					doRequests(tftpURL, count, k8s1Name)
+					if testFromOutside {
+						doRequestsFromThirdHost(httpURL, count, false)
+						doRequestsFromThirdHost(tftpURL, count, false)
+					}
 
 					httpURL = getHTTPLink(secondaryK8s2IPv4, data.Spec.Ports[0].NodePort)
 					tftpURL = getTFTPLink(secondaryK8s2IPv4, data.Spec.Ports[1].NodePort)
 					doRequests(httpURL, count, k8s2Name)
 					doRequests(tftpURL, count, k8s2Name)
+					if testFromOutside {
+						doRequestsFromThirdHost(httpURL, count, false)
+						doRequestsFromThirdHost(tftpURL, count, false)
+					}
 				}
 
 				// Ensure the NodePort cannot be bound from any redirected address
@@ -726,7 +742,7 @@ var _ = Describe("K8sServicesTest", func() {
 		}
 
 		SkipItIf(helpers.RunsWithoutKubeProxy, "Tests NodePort (kube-proxy)", func() {
-			testNodePort(false, false)
+			testNodePort(false, false, false)
 		})
 
 		SkipItIf(helpers.RunsWithoutKubeProxy, "Tests NodePort (kube-proxy) with externalTrafficPolicy=Local", func() {
@@ -755,7 +771,7 @@ var _ = Describe("K8sServicesTest", func() {
 
 				It("Tests NodePort with L7 Policy", func() {
 					applyPolicy(demoPolicy)
-					testNodePort(false, false)
+					testNodePort(false, false, false)
 				})
 			})
 
@@ -789,7 +805,7 @@ var _ = Describe("K8sServicesTest", func() {
 					})
 
 					It("Tests NodePort", func() {
-						testNodePort(true, false)
+						testNodePort(true, false, helpers.ExistNodeWithoutCilium())
 					})
 
 					It("Tests NodePort with externalTrafficPolicy=Local", func() {
@@ -810,7 +826,7 @@ var _ = Describe("K8sServicesTest", func() {
 								"global.nodePort.device": fmt.Sprintf(`'{%s,%s}'`, privateIface, helpers.SecondaryIface),
 							})
 
-							testNodePort(true, true)
+							testNodePort(true, true, helpers.ExistNodeWithoutCilium())
 						})
 				})
 
@@ -823,7 +839,7 @@ var _ = Describe("K8sServicesTest", func() {
 					})
 
 					It("Tests NodePort", func() {
-						testNodePort(true, false)
+						testNodePort(true, false, helpers.ExistNodeWithoutCilium())
 					})
 
 					It("Tests NodePort with externalTrafficPolicy=Local", func() {
@@ -844,7 +860,7 @@ var _ = Describe("K8sServicesTest", func() {
 								"global.nodePort.device": fmt.Sprintf(`'{%s,%s}'`, privateIface, helpers.SecondaryIface),
 							})
 
-							testNodePort(true, true)
+							testNodePort(true, true, helpers.ExistNodeWithoutCilium())
 						})
 
 					SkipContextIf(helpers.DoesNotExistNodeWithoutCilium, "Tests with MetalLB", func() {
