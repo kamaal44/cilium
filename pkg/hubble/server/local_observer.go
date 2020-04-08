dc6f52517e91e281522a85df584aba4c772aef33
Sebastian Wicki <sebastian@isovalent.com>
2020-04-08 15:30:18 +0200

    hubble/server: Populate total number of flows and uptime in status
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/hubble/server/local_observer.go
+++ pkg/hubble/server/local_observer.go
@@ -84,6 +84,12 @@ type LocalObserverServer struct {
 	payloadParser *parser.Parser
 
 	opts serveroption.Options
+
+	// startTime is the time when this instance was started
+	startTime time.Time
+
+	// numObservedFlows counts how many flows have been observed
+	numObservedFlows uint64
 }
 
 // NewLocalServer returns a new local observer server.
@@ -112,6 +118,7 @@ func NewLocalServer(
 		stopped:       make(chan struct{}),
 		eventschan:    make(chan *observer.GetFlowsResponse, 100), // option here?
 		payloadParser: payloadParser,
+		startTime:     time.Now(),
 		opts:          opts,
 	}
 
@@ -161,6 +168,7 @@ nextEvent:
 			}
 		}
 
+		s.numObservedFlows++
 		// FIXME: Convert metrics into an OnDecodedFlow function
 		metrics.ProcessFlow(flow)
 
@@ -212,8 +220,10 @@ func (s *LocalObserverServer) ServerStatus(
 	ctx context.Context, req *observer.ServerStatusRequest,
 ) (*observer.ServerStatusResponse, error) {
 	return &observer.ServerStatusResponse{
-		MaxFlows: s.GetRingBuffer().Cap(),
-		NumFlows: s.GetRingBuffer().Len(),
+		MaxFlows:  s.GetRingBuffer().Cap(),
+		NumFlows:  s.GetRingBuffer().Len(),
+		SeenFlows: s.numObservedFlows,
+		UptimeNs:  uint64(time.Since(s.startTime).Nanoseconds()),
 	}, nil
 }
 
